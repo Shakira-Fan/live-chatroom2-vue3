af000fdba4b3e408e318d3ab6bff044b7a539927
Shakira0612 <105627687+Shakira0612@users.noreply.github.com>
2022-10-26 11:32:37 +0800

    create a SignUp & LogIn Form

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="welcome container">
     <p>Welcome</p>
+    <SignupForm/>
+    <LoginForm/>
   </div>
 </template>
 
 <script>
+import SignupForm from "@/components/SignupForm";
+import LoginForm from "@/components/LoginForm";
 export default {
-  name: "WelcomeView"
+  name: "WelcomeView",
+  components: {LoginForm, SignupForm}
 }
 </script>
 
-<style scoped>
+<style>
   .welcome{
     text-align: center;
     padding: 20px 0;
+  }
+  /* form styles */
+  .welcome form {
+    width: 300px;
+    margin: 20px auto;
+  }
+
+  .welcome label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+
+  .welcome input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
+  }
+
+  .welcome span {
+    font-weight: bold;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+
+  .welcome button {
+    margin: 20px auto;
   }
 </style>
