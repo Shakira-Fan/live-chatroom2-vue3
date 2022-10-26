cad4000c1e16b5ad0118abddfa0f33853241e649
Shakira0612 <105627687+Shakira0612@users.noreply.github.com>
2022-10-26 17:30:05 +0800

    CHALLENGE - Watching the Current User

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import './assets/main.css'
 
 //import firebase auth service
+//即便重新渲染畫面，也會先觸發requireAuth，這樣就不會造成又回到welcomeView畫面
 import {projectAuth} from "@/firebase/config";
 
 let app
