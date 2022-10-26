e70e547d095a7888055944e05f9c7c9a77662d90
Shakira0612 <105627687+Shakira0612@users.noreply.github.com>
2022-10-26 16:40:20 +0800

    Route Guards(for Unauth Users)

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import WelcomeView from "@/views/WelcomeView";
 import ChatroomView from "@/views/ChatroomView";
+import {projectAuth} from "@/firebase/config";
+
+//auth guard
+const requireAuth = (to, from, next) => {
+    let user = projectAuth.currentUser
+    console.log('current user in auth guard:', user)
+    if (!user) {
+        next({name: 'welcomeView'})
+    } else {
+        next()
+    }
+}
 
 const routes = [
-  {
-    path: '/',
-    name: 'WelcomeView',
-    component: WelcomeView
-  },
-  {
-    path: '/chatroom',
-    name: 'ChatroomView',
-    component: ChatroomView
-  },
+    {
+        path: '/',
+        name: 'WelcomeView',
+        component: WelcomeView
+    },
+    {
+        path: '/chatroom',
+        name: 'ChatroomView',
+        component: ChatroomView,
+        beforeEnter: requireAuth
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
