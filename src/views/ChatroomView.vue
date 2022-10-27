fe9e53b2284bbb671bcd15720cdb6c8e4cc36424
Shakira0612 <105627687+Shakira0612@users.noreply.github.com>
2022-10-27 09:15:10 +0800

    CHALLENGE - Watching the Current User2

--- src/views/ChatroomView.vue
+++ src/views/ChatroomView.vue
@@ -1,14 +1,27 @@
 <template>
-<div class="container">
-  <NavBar/>
-</div>
+  <div class="container">
+    <NavBar/>
+  </div>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar";
+import getUser from "@/composables/getUser";
+import {watch} from "vue";
+import {useRouter} from "vue-router";
+
 export default {
   name: "ChatroomView",
-  components: {NavBar}
+  components: {NavBar},
+  setup() {
+    const {user} = getUser()
+    const router = useRouter()
+    watch(user, () => {
+      if (!user.value) {
+        router.push({name: 'WelcomeView'})
+      }
+    })
+  }
 }
 </script>
 
